94c1eeb3332cb90334f4997c42b6c6dc7e5e5996
林桂涛 <852951042@qq.com>
2022-08-23 16:48:32 +0800

    test: nuxt3初体验以及了解useAsyncData

---
+++ nuxt3-app-start-demo/pages/home.vue
@@ -0,0 +1,28 @@
+<script setup>
+// key
+const { data } = await useAsyncData("ssr", () =>
+  $fetch("http://127.0.0.1:3008/nuxt3/api/getList?pageSize=30&pageNo=1")
+);
+console.log("🚀【result】", data.value);
+const dataList = data.value.data;
+</script>
+
+<template>
+  <div>Page Visils:</div>
+  <div v-for="(item, index) in dataList" class="list-item">
+    <span>{{ item.chat_cnt }}</span>
+    <span>{{ item.message_cnt }}</span>
+    <span>{{ item.new_apply_cnt }}</span>
+    <span>{{ item.stat_time }}</span>
+  </div>
+</template>
+
+<style scoped>
+.list-item {
+  display: flex;
+  align-items: center;
+}
+.list-item span {
+  margin-right: 20px;
+}
+</style>
